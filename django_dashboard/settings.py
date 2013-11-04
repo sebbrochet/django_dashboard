972963977d6bf772f583219b32e018ee017d324e
sebbrochet <contact@sebbrochet.com>
2013-11-04 14:08:47 +0100

    Activate Admin interface and restapi application

--- django_dashboard/settings.py
+++ django_dashboard/settings.py
@@ -125,11 +125,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'south',
+    'restapi',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
