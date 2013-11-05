f6120f95c4c8f8142ea6b6d739aec52b5cb215e5
sebbrochet <contact@sebbrochet.com>
2013-11-05 16:51:54 +0100

    Enable PUT/POST/DELETE

--- restapi/api.py
+++ restapi/api.py
@@ -1,22 +1,27 @@
 from tastypie.resources import ModelResource
 from restapi.models import Project, Environment, Status, Event
+from tastypie.authorization import Authorization
 
 class ProjectResource(ModelResource):
     class Meta:
         queryset = Project.objects.all()
         resource_name = 'projects'
+        authorization= Authorization()
 
 class EnvironmentResource(ModelResource):
     class Meta:
         queryset = Environment.objects.all()
         resource_name = 'environments'
+        authorization= Authorization()
 
 class EventResource(ModelResource):
     class Meta:
         queryset = Event.objects.all()
         resource_name = 'events'
+        authorization= Authorization()
 
 class StatusResource(ModelResource):
     class Meta:
         queryset = Status.objects.all()
         resource_name = 'statuses'
+        authorization= Authorization()
