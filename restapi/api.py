9a04b68ed3244aba78ce9619ed3c0e5e9ffa154e
sebbrochet <contact@sebbrochet.com>
2013-11-07 17:31:28 +0100

    Creation of objects via REST API is OK now

--- restapi/api.py
+++ restapi/api.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from restapi.models import Project, Environment, Status, Event
 from tastypie.authorization import Authorization
+from tastypie import fields
 
 class ProjectResource(ModelResource):
     class Meta:
@@ -9,19 +10,23 @@ class ProjectResource(ModelResource):
         authorization= Authorization()
 
 class EnvironmentResource(ModelResource):
+    project = fields.ForeignKey(ProjectResource, 'project')
     class Meta:
         queryset = Environment.objects.all()
         resource_name = 'environments'
-        authorization= Authorization()
-
-class EventResource(ModelResource):
-    class Meta:
-        queryset = Event.objects.all()
-        resource_name = 'events'
         authorization= Authorization()
 
 class StatusResource(ModelResource):
     class Meta:
         queryset = Status.objects.all()
         resource_name = 'statuses'
+        authorization= Authorization()
+
+class EventResource(ModelResource):
+    environment = fields.ForeignKey(EnvironmentResource, 'environment')
+    status = fields.ForeignKey(StatusResource, 'status', null=True, blank=True)
+
+    class Meta:
+        queryset = Event.objects.all()
+        resource_name = 'events'
         authorization= Authorization()
--- restapi/models.py
+++ restapi/models.py
@@ -14,7 +14,7 @@ class Environment(models.Model):
     creation_date = models.DateTimeField(default=datetime.datetime.now)
     name = models.CharField(max_length=200)
     description = models.TextField()
-    project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project, related_name = 'environments')
     
     def __unicode__(self):
         return self.name
@@ -30,8 +30,8 @@ class Event(models.Model):
     date = models.DateTimeField(default=datetime.datetime.now)
     subject = models.CharField(max_length=200)
     message = models.TextField()
-    environment = models.ForeignKey(Environment) 
-    status = models.ForeignKey(Status, blank=True, null=True)
+    environment = models.ForeignKey(Environment, related_name = 'events') 
+    status = models.ForeignKey(Status, blank=True, null=True, related_name = 'events')
 
     def __unicode__(self):
         return self.subject
